cc83f133b8eb54f124bf83cef1ce16f643ca35f4
jka4 <plotnik1295@gmail.com>
2021-02-18 17:58:13 +0200

    update

---
+++ src/Store/store.js
@@ -0,0 +1,57 @@
+// @ts-nocheck
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import thunk from 'redux-thunk';
+
+import { saveState, loadState } from './localStorage';
+
+import throttle from 'lodash.throttle';
+import uniqBy from 'lodash.uniqby';
+
+let defaultState = {
+  heroesListPage: [],
+  heroesListCurrentPage: 1,
+  pageIsFetching: false,
+  detailedPage: {},
+  favorites: [],
+  favoritesList: [],
+};
+
+function rootReducer(state = defaultState, action) {
+  const payload = action.payload;
+
+  switch (action.type) {
+    case 'GET_PAGE_HEROES':
+      return { ...state, heroesListPage: [...payload] };
+    case 'PAGE_IS_FETCHING':
+      return { ...state, pageIsFetching: payload };
+    case 'SET_HEROES_LIST_CURRENT_PAGE':
+      return { ...state, heroesListCurrentPage: payload };
+    case 'SET_DETAILED_PAGE':
+      return { ...state, detailedPage: payload };
+    case 'ADD_TO_FAVORITES':
+      return { ...state, favorites: payload };
+    case 'SET_FAVORITES_LIST':
+      return { ...state, favoritesList: uniqBy([...state.favoritesList, payload], 'name') };
+    default:
+      return state;
+  }
+}
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+  middlewares.push(logger);
+}
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: loadState(),
+  middleware: middlewares,
+});
+
+store.subscribe(throttle(() => saveState(store.getState()), 1000));
+
+export default store;
